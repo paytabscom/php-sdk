f3800c8a0969ddea4b5c4215676eba33cea03af6
Wajih Kammoun <wajih.km@gmail.com>
2021-09-21 16:35:50 +0400

    Associated methods: get methods related to Currency

--- paytabs_core.php
+++ paytabs_core.php
@@ -2,10 +2,10 @@
 
 /**
  * PayTabs v2 PHP SDK
- * Version: 2.3.1
+ * Version: 2.3.2
  */
 
-define('PAYTABS_SDK_VERSION', '2.3.1');
+define('PAYTABS_SDK_VERSION', '2.3.2');
 
 
 abstract class PaytabsHelper
@@ -59,7 +59,7 @@ abstract class PaytabsHelper
         return false;
     }
 
-    static function getCardPayments($international_only = false)
+    static function getCardPayments($international_only = false, $currency = null)
     {
         $methods = [];
 
@@ -67,7 +67,13 @@ abstract class PaytabsHelper
 
         foreach (PaytabsApi::PAYMENT_TYPES as $key => $value) {
             if (in_array($group, $value['groups'])) {
-                $methods[] = $value['name'];
+                if ($currency) {
+                    if ($value['currencies'] == null || in_array($currency, $value['currencies'])) {
+                        $methods[] = $value['name'];
+                    }
+                } else {
+                    $methods[] = $value['name'];
+                }
             }
         }
         return $methods;
@@ -459,16 +465,16 @@ class PaytabsRequestHolder extends PaytabsHolder
 
     //
 
-    public function set01PaymentCode($code, $allow_associated_methods = true)
+    public function set01PaymentCode($code, $allow_associated_methods = true, $currency = null)
     {
         $codes = [$code];
 
         if (PaytabsHelper::isCardPayment($code)) {
             if ($allow_associated_methods) {
                 if (PaytabsHelper::isCardPayment($code, true)) {
-                    $other_cards = PaytabsHelper::getCardPayments(false);
+                    $other_cards = PaytabsHelper::getCardPayments(false, $currency);
                 } else {
-                    $other_cards = PaytabsHelper::getCardPayments(true);
+                    $other_cards = PaytabsHelper::getCardPayments(true, $currency);
                 }
                 $codes = array_unique(array_merge($other_cards, $codes));
             }
