406b8576d6542664ea43c2fe876f564a82c1792b
Wajih <wajih.km@gmail.com>
2023-02-10 18:22:40 +0400

    Accept prefixed method codes and handle them

--- paytabs_core.php
+++ paytabs_core.php
@@ -2,7 +2,7 @@
 
 /**
  * PayTabs v2 PHP SDK
- * Version: 2.11.0
+ * Version: 2.11.1
  * PHP >= 7.0.0
  */
 
@@ -603,6 +603,12 @@ abstract class PaytabsBasicHolder extends PaytabsHolder
         }
 
         // 'creditcard' => ['creditcard', 'mada', 'omannet', 'meeza']
+
+        foreach ($codes as &$code) {
+            if (substr($code, 0, 3) === "pt_") {
+                $code = substr($code, 3);
+            }
+        }
 
         $this->payment_code = ['payment_methods' => $codes];
 
