13c750b03eb59637533f3865aaba257144a46db1
Walaa Elsaeed <79043296+Walaa-Elsaeed-93@users.noreply.github.com>
2024-06-05 21:04:18 +0300

    Update sdk version

--- paytabs_core.php
+++ paytabs_core.php
@@ -2,11 +2,11 @@
 
 /**
  * PayTabs v2 PHP SDK
- * Version: 2.21.2
+ * Version: 2.21.3
  * PHP >= 7.0.0
  */
 
-define('PAYTABS_SDK_VERSION', '2.21.2');
+define('PAYTABS_SDK_VERSION', '2.21.3');
 
 define('PAYTABS_DEBUG_FILE_NAME', 'debug_paytabs.log');
 define('PAYTABS_DEBUG_SEVERITY', ['Info', 'Warning', 'Error']);
