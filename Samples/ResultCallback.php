af8aa722dbb170cdf95c3bebd7919103c8e32517
Wajih <wajih.km@gmail.com>
2025-01-29 09:41:44 +0000

    Update the Callback

--- Samples/ResultCallback.php
+++ Samples/ResultCallback.php
@@ -3,10 +3,10 @@
 use Paytabs\Sdk\Paytabs;
 use Paytabs\Sdk\Response\Responses\Webhook\TransactionResult\Callback;
 
-$payload = '{"tran_ref":"TST2501402204249","merchant_id":1842,"profile_id":47170,"cart_id":"c01","cart_description":"Test","cart_currency":"AED","cart_amount":"100.51","tran_currency":"AED","tran_total":"100.51","tran_type":"Sale","tran_class":"ECom","customer_details":{"name":"Wajih sdk","email":"wajih@mail.com","phone":"0522222222","street1":"nsr st","city":"Dubai","state":"DU","country":"AE","ip":"2001:8f8:1471:ccd:f90a:9c05:1be9:c1cb"},"shipping_details":{"name":"Wajih 2","street1":"No Address"},"payment_result":{"response_status":"A","response_code":"G61298","response_message":"Authorised","acquirer_ref":"TRAN0002.67866672.0011E2F5","cvv_result":" ","avs_result":" ","transaction_time":"2025-01-14T13:28:18Z"},"payment_info":{"payment_method":"Visa","card_type":"Credit","card_scheme":"Visa","payment_description":"4111 11## #### 1111","expiryMonth":11,"expiryYear":2033},"token":"2C4654BD67A3ED33C6B693FF628674B0","ipn_trace":"IPNS0002.67866672.0000A10E"}';
+$payload = '{"tran_ref":"TST2502902213573","merchant_id":1842,"profile_id":47170,"cart_id":"c01","cart_description":"Test","cart_currency":"AED","cart_amount":"101.51","tran_currency":"AED","tran_total":"101.51","tran_type":"Sale","tran_class":"ECom","customer_details":{"name":"Wajih SDK3","email":"wajih@mail.com","phone":"0522222222","street1":"nsr st","city":"Dubai","state":"DU","country":"AE","ip":"2001:8f8:1471:ccd:4c61:ab3:210b:c367"},"payment_result":{"response_status":"A","response_code":"G42967","response_message":"Authorised","acquirer_ref":"TRAN0001.6799F4F6.000101C9","cvv_result":" ","avs_result":" ","transaction_time":"2025-01-29T09:29:27Z"},"payment_info":{"payment_method":"Visa","card_type":"Credit","card_scheme":"Visa","payment_description":"4000 00## #### 0002","expiryMonth":11,"expiryYear":2033},"token":"2C4655BE67A3E537C6B593FF60817AB9","threeDSDetails":{"responseLevel":2,"responseStatus":4,"enrolled":"Y","paResStatus":"Y","eci":"05","cavv":"","ucaf":"VFNUMjUwMjkwMjIxMzU3MzI3YTc=","threeDSVersion":"Test/Simulation"},"ipn_trace":"IPNS0001.6799F4F7.00000950","paymentChannel":"PHP SDK"}';
 
 $headers = [
-    'Signature' => '4a0275cb696e5bcd55fc60fe170631299ee4e3e799bf35c440430b16c89b75ec'
+    'Signature' => '5cb373805f3ac2b1bc55b4ea41e0ea1b585e31f970977780370e2a5f06a7bd2c'
 ];
 
 //
