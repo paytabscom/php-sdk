6759f2304371188aaf6347ba4e3ba031b75b3281
Wajih <wajih.km@gmail.com>
2025-03-14 11:51:03 +0000

    Code format

--- src/Request/Payload/Paytabs/PaytabsBuilder.php
+++ src/Request/Payload/Paytabs/PaytabsBuilder.php
@@ -4,7 +4,6 @@ namespace Paytabs\Sdk\Request\Payload\Paytabs;
 
 use Paytabs\Sdk\Paytabs;
 use Paytabs\Sdk\Request\Payload\AbstractBuilder;
-use Paytabs\Sdk\Request\Payload\Paytabs\PaytabsPayload;
 
 abstract class PaytabsBuilder extends AbstractBuilder
 {
