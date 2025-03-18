5a72154ac45009bf7c8b2d0ee2db3f45647a9354
Walaa-Elsaeed-93 <w.elsaeed@paytabs.com>
2025-03-18 22:15:36 +0000

    managed form holder

---
+++ src/Request/Payload/Parts/ManagedFormToken.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Paytabs\Sdk\Request\Payload\Parts;
+
+class ManagedFormToken extends AbstractPart
+{
+    private string $paymentToken;
+
+    public function __construct(
+        string $paymentToken,
+    ) {
+        $this->paymentToken = $paymentToken;
+    }
+
+    public function build(): array
+    {
+        return [
+            'payment_token' => $this->paymentToken
+        ];
+    }
+}
---
+++ src/Request/Payload/Payloads/ManagedForm.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Paytabs\Sdk\Request\Payload\Payloads;
+
+use Paytabs\Sdk\Request\Payload\Parts\ManagedFormToken;
+
+
+class ManagedForm extends PrimaryPayment
+{
+    public function buildPaymentToken(string $paymentToken)
+     {
+         $this->product->buildBody(
+             new ManagedFormToken($paymentToken)
+         );
+ 
+         return $this;
+     }
+}
