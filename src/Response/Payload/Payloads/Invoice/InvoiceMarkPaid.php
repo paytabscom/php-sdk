8465afa12259416add1063a3a7748136b709b460
Wajih <wajih.km@gmail.com>
2025-03-28 07:51:59 +0000

    Fix response members mandatory Update Todo list

--- src/Response/Payload/Payloads/Invoice/InvoiceMarkPaid.php
+++ src/Response/Payload/Payloads/Invoice/InvoiceMarkPaid.php
@@ -6,15 +6,15 @@ use Paytabs\Sdk\Response\Payload\Payloads\Paytabs;
 
 class InvoiceMarkPaid extends Paytabs
 {
-    public ?string $profile_id;
+    public string $profile_id;
 
-    public ?string $invoice_id;
+    public string $invoice_id;
 
-    public ?string $invoice_currency;
-    public ?string $invoice_total;
+    public string $invoice_currency;
+    public string $invoice_total;
 
-    public ?string $pay_method;
-    public ?string $pay_description;
+    public string $pay_method;
+    public string $pay_description;
 
-    public ?string $tran_ref;
+    public string $tran_ref;
 }
