fd7e317d0a8d109cde86553a9ce038bfdf8fdd48
Wajih <wajih.km@gmail.com>
2025-11-26 10:34:37 +0000

    Add Unit tests for Methods Factory

--- tests/ManagedFormTest.php
+++ tests/ManagedFormTest.php
@@ -6,11 +6,9 @@ use PHPUnit\Framework\TestCase;
 use Paytabs\Sdk\Request\Payload\Payloads\ManagedForm;
 use Paytabs\Sdk\Request\Payload\Parts\ManagedFormToken;
 
-
 final class ManagedFormTest extends TestCase
 {
-
-    public function testBuildPaymentToken() :void
+    public function testBuildPaymentToken(): void
     {
         // Arrange
         $paymentToken = 'test_payment_token_123';
--- tests/PaymentMethodsTest.php
+++ tests/PaymentMethodsTest.php
@@ -3,9 +3,12 @@
 declare(strict_types=1);
 
 use Paytabs\Sdk\PaymentMethod\AbstractMethod;
+use Paytabs\Sdk\PaymentMethod\Methods\ApplePay;
 use Paytabs\Sdk\PaymentMethod\Methods\Card;
+use Paytabs\Sdk\PaymentMethod\Methods\PayTabsAll;
 use Paytabs\Sdk\PaymentMethod\Methods\Sadad;
 use Paytabs\Sdk\PaymentMethod\MethodsFactory;
+use Paytabs\Sdk\Request\Payload\Parts\PaymentMethods;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -80,5 +83,56 @@ final class PaymentMethodsTest extends TestCase
             $method = MethodsFactory::createMethodByUnique($code);
             self::assertNull($method);
         }
+    }
+
+    public function testFactoryConvenienceMethods(): void
+    {
+        $all = MethodsFactory::createPayTabsAllMethod();
+        self::assertInstanceOf(AbstractMethod::class, $all);
+        self::assertInstanceOf(PayTabsAll::class, $all);
+        self::assertIsString($all::CODE);
+        self::assertIsString($all::PT_CODE);
+        self::assertIsArray($all::supportedCurrencies());
+
+        $card = MethodsFactory::createCardMethod();
+        self::assertInstanceOf(AbstractMethod::class, $card);
+        self::assertInstanceOf(Card::class, $card);
+        self::assertIsString($card::CODE);
+        self::assertIsString($card::PT_CODE);
+        self::assertIsArray($card::supportedCurrencies());
+
+        $apple = MethodsFactory::createApplePayMethod();
+        self::assertInstanceOf(AbstractMethod::class, $apple);
+        self::assertInstanceOf(ApplePay::class, $apple);
+        self::assertIsString($apple::CODE);
+        self::assertIsString($apple::PT_CODE);
+        self::assertIsArray($apple::supportedCurrencies());
+
+        $sadad = MethodsFactory::createSadadMethod();
+        self::assertInstanceOf(AbstractMethod::class, $sadad);
+        self::assertInstanceOf(Sadad::class, $sadad);
+        self::assertIsArray($sadad::supportedCurrencies());
+    }
+
+    public function testPaymentMethodsBuilderIncludeExclude(): void
+    {
+        $methods = PaymentMethods::init([MethodsFactory::createApplePayMethod(), 'card'])
+            ->includeMethod(MethodsFactory::createCardMethod())
+            ->includeMethods(['fawry', MethodsFactory::createSadadMethod()])
+            ->excludeMethod('sadad')
+            ->excludeMethod(MethodsFactory::createFawryMethod());
+
+        self::assertIsObject($methods);
+
+        $built = $methods->build();
+        self::assertIsArray($built);
+        self::assertNotEmpty($built);
+
+        // Basic sanity checks: ensure excluded methods are not present by key or value when possible
+        $flatten = json_encode($built);
+        self::assertIsString($flatten);
+        self::assertStringContainsString('-sadad', strtolower($flatten));
+        // Ensure at least one included method is present
+        self::assertStringContainsString('card', strtolower($flatten));
     }
 }
