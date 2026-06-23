cd85aa0135fb564d6dc54b61fab387ec86517719
Wajih <wajih.km@gmail.com>
2026-06-23 10:39:03 +0000

    Refactor ResultBrowser and ResultCallback to enforce profile variable checks and update payload examples for clarity

--- Samples/ResultBrowser.php
+++ Samples/ResultBrowser.php
@@ -1,33 +1,46 @@
 <?php
 
 use Paytabs\Sdk\Paytabs;
+use Paytabs\Sdk\Profile\Profile;
 use Paytabs\Sdk\Response\Responses\Webhook\TransactionResult\BrowserAsGet;
 use Paytabs\Sdk\Response\Responses\Webhook\TransactionResult\BrowserAsPost;
 
-$urlResult = 'mode=return&cartId=c01&respCode=G53384&respStatus=A&signature=edfb82b2db4310c8632428e763b5a2b512967b9cdb94bffb9820a7dead19938e&tranRef=TST2501402204131';
+/**
+ * @var Profile $profile
+ */
 
-// $response = BrowserAsGet::init($localParams);
+if (!isset($profile)) {
+    throw new \RuntimeException('Required variable is not set: $profile');
+}
 
+//
+
+// URL query string, Format (sample): APP_URL?cartId=CART_ID$respCode=RESP_CODE&respMessage=RESP_MESSAGE&respStatus=RESP_STATUS&tranRef=TRAN_REF&signature=SIGNATURE
+$urlResult = 'result=1&mode=return&cartId=cart01&respCode=G07626&respStatus=A&signature=d404cc5e9f50ca7e5fff316f7b1ac5e6af12803302e2cbac555ddb6f7b4447b9&tranRef=TST2617402946412';
+
+$getArray1 = [];
 parse_str($urlResult, $getArray1);
-$response1 = BrowserAsGet::initWith($getArray1);
+$response1 = BrowserAsGet::initWith($getArray1, ['mode', 'result']);
 
 $response1->setProfile($profile);
 
-Paytabs::getLogger()->debug('Return Payload: ', [
+Paytabs::getLogger()->debug('Return Payload (As GET): ', [
     'isGenuine' => $response1->isGenuine() ? 'Yes' : 'No',
     'Response' => $response1->getPayload()->getMapped(),
 ]);
 
-$getResult = 'acquirerMessage=&acquirerRRN=&cartId=c01&customerEmail=wajih%40mail.com&respCode=G42967&respMessage=Authorised&respStatus=A&signature=7328ebcbd1708a14ed9ec34731b017b569dec45170fd2aa6abc113018defe911&token=2C4655BE67A3E537C6B593FF60817AB9&tranRef=TST2502902213573';
+//
 
-// $response = BrowserAsPost::init();
+// POST data, Format (sample): acquirerMessage=&acquirerRRN=&cartId=CART_ID&customerEmail=CUSTOMER_EMAIL&respCode=RESP_CODE&respMessage=RESP_MESSAGE&respStatus=RESP_STATUS&signature=SIGNATURE&token=TOKEN&tranRef=TRAN_REF
+$getResult = 'acquirerMessage=&acquirerRRN=&cartId=cart01&customerEmail=integrations%40paytabs.com&respCode=321&respMessage=Cancelled&respStatus=C&signature=cfac02a6b6d9081b4da2a8e2e46bde5ad42b00ef09dd856985b18851f771e8c5&token=&tranRef=TST2617402946419';
 
+$getArray2 = [];
 parse_str($getResult, $getArray2);
-$response2 = BrowserAsPost::initWith($getArray2, ['mode', 'result']);
+$response2 = BrowserAsPost::initWith($getArray2);
 
 $response2->setProfile($profile);
 
-Paytabs::getLogger()->debug('Return Payload: ', [
+Paytabs::getLogger()->debug('Return Payload (As POST): ', [
     'isGenuine' => $response2->isGenuine() ? 'Yes' : 'No',
     'Response' => $response2->getPayload()->getMapped(),
 ]);
--- Samples/ResultCallback.php
+++ Samples/ResultCallback.php
@@ -4,13 +4,20 @@ use Paytabs\Sdk\Paytabs;
 use Paytabs\Sdk\Profile\Profile;
 use Paytabs\Sdk\Response\Responses\Webhook\TransactionResult\Callback;
 
-$payload = '{"tran_ref":"TST2502902213573","merchant_id":1842,"profile_id":47170,"cart_id":"c01","cart_description":"Test","cart_currency":"AED","cart_amount":"101.51","tran_currency":"AED","tran_total":"101.51","tran_type":"Sale","tran_class":"ECom","customer_details":{"name":"Wajih SDK3","email":"wajih@mail.com","phone":"0522222222","street1":"nsr st","city":"Dubai","state":"DU","country":"AE","ip":"2001:8f8:1471:ccd:4c61:ab3:210b:c367"},"payment_result":{"response_status":"A","response_code":"G42967","response_message":"Authorised","acquirer_ref":"TRAN0001.6799F4F6.000101C9","cvv_result":" ","avs_result":" ","transaction_time":"2025-01-29T09:29:27Z"},"payment_info":{"payment_method":"Visa","card_type":"Credit","card_scheme":"Visa","payment_description":"4000 00## #### 0002","expiryMonth":11,"expiryYear":2033},"token":"2C4655BE67A3E537C6B593FF60817AB9","threeDSDetails":{"responseLevel":2,"responseStatus":4,"enrolled":"Y","paResStatus":"Y","eci":"05","cavv":"","ucaf":"VFNUMjUwMjkwMjIxMzU3MzI3YTc=","threeDSVersion":"Test/Simulation"},"ipn_trace":"IPNS0001.6799F4F7.00000950","paymentChannel":"PHP SDK"}';
+/**
+ * @var Profile $profile
+ */
+
+if (!isset($profile)) {
+    throw new \RuntimeException('Required variable is not set: $profile');
+}
+
+// IPN payload, Format (sample): JSON body with 'Signature' header
+$payload = '{"tran_ref":"TST2617402946403","previous_tran_ref":"TST2617402946400","merchant_id":2550,"profile_id":48214,"cart_id":"ca-03","cart_description":"Test","cart_currency":"SAR","cart_amount":"700.00","tran_currency":"SAR","tran_total":"700.00","tran_type":"Sale","tran_class":"C/Auth","customer_ref":"customer-ref-1","customer_details":{"name":"Integrations SDK3","email":"integrations@paytabs.com","phone":"0522222222","street1":"nsr st","city":"Dubai","state":"DU","country":"AE"},"payment_result":{"response_status":"A","response_code":"G07062","response_message":"Authorised","acquirer_ref":"TRAN0201.6A3A5256.00109416","cvv_result":" ","avs_result":" ","transaction_time":"2026-06-23T09:31:02Z"},"payment_info":{"payment_method":"Visa","card_type":"Credit","card_scheme":"Visa","payment_description":"4111 11## #### 1111","expiryMonth":11,"expiryYear":2033},"token":"2C4656BF67A3E832C6BE93FC61847DBC","user_defined":{"udf1":"udf_1","udf4":"udf_4","udf8":"udf_8"},"ipn_trace":"IPNS0201.6A3A5256.000136D4","paymentChannel":"PHP SDK"}';
 
 $headers = [
-    'Signature' => '5cb373805f3ac2b1bc55b4ea41e0ea1b585e31f970977780370e2a5f06a7bd2c',
+    'Signature' => 'cf9a70586cb63f1f11837196562ecced04bf9e39e88f884a23b9a7aec94865af',
 ];
-
-/** @var Profile $profile */
 
 $ipnResponse = Callback::initWith($payload, $headers);
 $ipnResponse->setProfile($profile);
