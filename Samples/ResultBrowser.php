dab4bedc807bdbdaee3ae5a69a757d5acce10df5
Wajih <wajih.km@gmail.com>
2026-06-24 08:09:44 +0000

    Re arrange sample browser result

--- Samples/ResultBrowser.php
+++ Samples/ResultBrowser.php
@@ -16,6 +16,22 @@ if (!isset($profile)) {
 
 //
 
+// POST data, Format (sample): acquirerMessage=&acquirerRRN=&cartId=CART_ID&customerEmail=CUSTOMER_EMAIL&respCode=RESP_CODE&respMessage=RESP_MESSAGE&respStatus=RESP_STATUS&signature=SIGNATURE&token=TOKEN&tranRef=TRAN_REF
+$getResult = 'acquirerMessage=&acquirerRRN=&cartId=cart01&customerEmail=integrations%40paytabs.com&respCode=321&respMessage=Cancelled&respStatus=C&signature=REPLACE_WITH_VALID_SIGNATURE&token=&tranRef=TST_REFERENCE_PLACEHOLDER';
+
+$getArray2 = [];
+parse_str($getResult, $getArray2);
+$response2 = BrowserAsPost::initWith($getArray2);
+
+$response2->setProfile($profile);
+
+Paytabs::getLogger()->debug('Return Payload (As POST): ', [
+    'isGenuine' => $response2->isGenuine() ? 'Yes' : 'No',
+    'Response' => $response2->getPayload()->getMapped(),
+]);
+
+//
+
 // URL query string, Format (sample): APP_URL?cartId=CART_ID$respCode=RESP_CODE&respMessage=RESP_MESSAGE&respStatus=RESP_STATUS&tranRef=TRAN_REF&signature=SIGNATURE
 $urlResult = 'result=1&mode=return&cartId=cart01&respCode=G00000&respStatus=A&signature=REPLACE_WITH_VALID_SIGNATURE&tranRef=TST_REFERENCE_PLACEHOLDER';
 
@@ -30,18 +46,3 @@ Paytabs::getLogger()->debug('Return Payload (As GET): ', [
     'Response' => $response1->getPayload()->getMapped(),
 ]);
 
-//
-
-// POST data, Format (sample): acquirerMessage=&acquirerRRN=&cartId=CART_ID&customerEmail=CUSTOMER_EMAIL&respCode=RESP_CODE&respMessage=RESP_MESSAGE&respStatus=RESP_STATUS&signature=SIGNATURE&token=TOKEN&tranRef=TRAN_REF
-$getResult = 'acquirerMessage=&acquirerRRN=&cartId=cart01&customerEmail=integrations%40paytabs.com&respCode=321&respMessage=Cancelled&respStatus=C&signature=REPLACE_WITH_VALID_SIGNATURE&token=&tranRef=TST_REFERENCE_PLACEHOLDER';
-
-$getArray2 = [];
-parse_str($getResult, $getArray2);
-$response2 = BrowserAsPost::initWith($getArray2);
-
-$response2->setProfile($profile);
-
-Paytabs::getLogger()->debug('Return Payload (As POST): ', [
-    'isGenuine' => $response2->isGenuine() ? 'Yes' : 'No',
-    'Response' => $response2->getPayload()->getMapped(),
-]);
