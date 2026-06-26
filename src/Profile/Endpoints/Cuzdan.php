419725ad1e044a9494c4f1ca42ce5cf20891d085
Wajih <wajih.km@gmail.com>
2026-06-26 11:39:45 +0000

    Add missing Endpoints (from v2)

---
+++ src/Profile/Endpoints/Cuzdan.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Paytabs\Sdk\Profile\Endpoints;
+
+use Paytabs\Sdk\Profile\AbstractEndpoint;
+
+final class Cuzdan extends AbstractEndpoint
+{
+    public const CODE = 'CUZDAN';
+    protected const TITLE = 'Cuzdan';
+    protected const URL = 'https://secure.cuzdan.az';
+}
