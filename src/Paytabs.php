ac9611ba8061016b7d7007b3822e52f42a987978
Wajih <wajih.km@gmail.com>
2026-08-12 12:20:36 +0000

    Update the version

--- src/Paytabs.php
+++ src/Paytabs.php
@@ -14,7 +14,7 @@ use Psr\Log\NullLogger;
 class Paytabs
 {
     // Version
-    public const VERSION = '3.2.5';
+    public const VERSION = '3.2.6';
 
     protected Profile $profile;
     protected Http $http;
